06140924f9cef23b7356dc637506e3592c30c336
Sarthak Gupta <32425257+sarthak1598@users.noreply.github.com>
2018-12-19 10:02:45 +0530

    Update WebsiteInfo.py

--- WebsiteInfo.py
+++ WebsiteInfo.py
@@ -3,11 +3,23 @@
 
 # module to connect with the whois database to retrieve the domain regarding information.
 
+#789sk.gupta@gmail.com 
 
 import whois
+import os
+
+import sys
 
 data = raw_input("Enter a domain name: ")
 
-w = whois.whois(data)
+w = whois.whois(data) # whois look-up of website and server ,owwner every basic information of domain.
 
-print w
+print w    # prints the information on the unix terminal 
+
+# happy info gathering:))
+
+
+
+
+
+
